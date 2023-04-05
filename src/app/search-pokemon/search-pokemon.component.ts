d8537d76794c9ed3c1fe40984b8bb11959680878
Timi <timi.ogunkeye@gmail.com>
2023-04-05 10:12:17 +0100

    add deboucePattern

--- src/app/search-pokemon/search-pokemon.component.ts
+++ src/app/search-pokemon/search-pokemon.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { debounceTime, distinctUntilChanged, filter, fromEvent, tap } from 'rxjs';
 import { UtilityService } from 'src/services/utility.service';
 
 @Component({
@@ -7,18 +8,34 @@ import { UtilityService } from 'src/services/utility.service';
   templateUrl: './search-pokemon.component.html',
   styleUrls: ['./search-pokemon.component.scss']
 })
-export class SearchPokemonComponent implements OnInit {
+export class SearchPokemonComponent implements OnInit, AfterViewInit {
 
   isSuggesting:boolean=false;
   suggestions:{name:string|undefined}[]=[];
+  @ViewChild('input', { static: true })
+  input!: ElementRef;
 
   constructor(private router: Router, private utility: UtilityService) { }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    fromEvent(this.input.nativeElement,'keyup')
+            .pipe(
+                filter(Boolean),
+                debounceTime(1500),
+                distinctUntilChanged()
+                )
+            .subscribe(()=>console.log(this.input.nativeElement.value));
+  }
 
   ngOnInit(): void {
   }
 
   searchPokemon($event: Event) {
     //do some debounce stuff here whilst typing to call giveSuggestions()
+    console.log("This is the event =>",$event);
+    
   }
 
   giveSuggestions(){
